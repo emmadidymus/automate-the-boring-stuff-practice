b1202c1a35c5b404f77da19e0d919a504beaf317
Emmanuel Didymus Sebastian <125865524+emmadidymus@users.noreply.github.com>
2025-07-10 14:48:43 +0530

    Add program to generate 5 random integers using random module
    This program uses the `random` module to print 5 random integers between 1 and 10. It demonstrates the use of `random.randint()` within a for loop and highlights basic usage of Python's standard library for randomness. Based on an example from "Automate the Boring Stuff with Python."

---
+++ printRandom.py
@@ -0,0 +1,8 @@
+import random
+for i in range(5):
+    print(random.randint(1,10))
+
+# This code generates and prints 5 random integers between 1 and 10.
+# It uses the random module to generate the integers.
+# Each time you run this code, it will produce different random numbers.
+# You can change the range by modifying the arguments of randint.
