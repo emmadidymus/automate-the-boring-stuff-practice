3e7e073ce1fd72524371288a7da30b507b77b912
Emmanuel Didymus Sebastian <125865524+emmadidymus@users.noreply.github.com>
2025-07-10 14:03:34 +0530

    Add program to calculate sum from 0 to 100 using for loop
    This program uses a for loop to calculate the total sum of numbers from 0 to 100. It demonstrates basic iteration, accumulation, and variable assignment. Based on a fundamental example from "Automate the Boring Stuff with Python."

---
+++ total.py
@@ -0,0 +1,8 @@
+total=0
+for num in range (101):
+    total = total + num
+print("The total is", total)
+
+## This code calculates the sum of all numbers from 0 to 100 using a for loop.
+# It initializes a variable `total` to 0, then iterates through each number in the range from 0 to 100,
+# adding each number to `total`. Finally, it prints the total sum.                                            
