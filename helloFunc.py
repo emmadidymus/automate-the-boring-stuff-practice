4ae9fcd99f946c86dd3e0fbdedafedff03ea876c
Emmanuel Didymus Sebastian <125865524+emmadidymus@users.noreply.github.com>
2025-07-12 12:54:21 +0530

    Add hello function with repeated calls
    Defines a simple function named `hello` that prints three greeting messages. The function is called three times to demonstrate reusability and basic function usage in Python.

---
+++ helloFunc.py
@@ -0,0 +1,12 @@
+def hello():
+    print("Howdy!")
+    print("Howdy! How are you?")
+    print("Hello there!")
+
+hello()
+hello()
+hello()
+
+# The above code defines a function `hello` that prints three messages. 
+# The function is called three times to display the messages.
+# The function can be reused to print the same messages multiple times.
